a8eec0cab9c2d5584c7ce40ca355e1a111752aa6
Trey Wheeler <trey.wheeler@daemongames.com>
2019-01-05 12:35:53 -0600

    WIP for Perry to pick up on his machine

---
+++ Campaign.Companion.Storage.Azure/NodeEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Campaign.Companion.Storage.Azure
+{
+    public class NodeEntity : TableEntity
+    {
+        public NodeEntity(int nodeId, string partitionKey)
+        {
+            this.PartitionKey = partitionKey;
+            this.RowKey = nodeId.ToString();
+        }
+
+        public NodeEntity() { }
+    }
+}
---
+++ Campaign.Companion.Storage.Azure/TableStorageNodeRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.Azure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Campaign.Companion.Storage.Azure
+{
+    public class TableStorageNodeRepository : INodeRepository
+    {
+        public TableStorageNodeRepository()
+        {
+            CreateTableIfNotExist();
+        }
+
+        public Node Add(Node node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int nodeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Node Read(int nodeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Node node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateTableIfNotExist()
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+            CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            CloudTable table = tableClient.GetTableReference("people");
+
+            // Create the table if it doesn't exist.
+            //table.CreateIfNotExists();
+        }
+    }
+}
